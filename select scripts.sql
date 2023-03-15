d288ae6af94eba7f130dd5284198cdbc36bf08fa
Timofey Prokhorov <prokhorovtd@yandex.ru>
2023-03-15 17:02:42 +0300

    some fixes

--- select scripts.sql
+++ select scripts.sql
@@ -2,19 +2,20 @@ SELECT g.name, count(ge.author_id) FROM genre g
 JOIN genre_author ge ON g.genre_id = ge.genre_id
 GROUP BY g.name;
 
-SELECT a.name, a.date, count(t.track_id) FROM album a
+SELECT count(t.track_id) FROM album a
 JOIN track t ON a.album_id = t.album_id
 WHERE a.date BETWEEN 2019 and 2020
-GROUP BY a.name, a.date;
+
 
 SELECT a.name, AVG(t.duration) FROM album a
 JOIN track t ON a.album_id = t.album_id
 GROUP BY a.name;
 
 SELECT au.name FROM author au
+where au.name not in(select au.name from author au
 JOIN author_album aa ON au.author_id = aa.author_id
 JOIN album a ON a.album_id = aa.album_id
-WHERE a.date != 2020; 
+WHERE a.date = 2020); 
 
 SELECT c.name FROM collector c
 JOIN collector_track ct ON c.collector_id = ct.collector_id
@@ -29,7 +30,7 @@ JOIN author_album aa ON a.album_id = aa.album_id
 JOIN author au ON au.author_id = aa.author_id
 JOIN genre_author ga ON ga.author_id = au.author_id
 JOIN genre g ON g.genre_id = ga.genre_id
-GROUP BY au.name, a.name
+GROUP BY a.name, au.name
 HAVING count(ga.genre_id) > 1;
 
 SELECT t.name FROM track t
